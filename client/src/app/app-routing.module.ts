ad4438e4915145789ea723355e81967fbc2a62b4
blabuduua <31963350+blabuduua@users.noreply.github.com>
2019-04-12 16:42:38 +0300

    make layouts and show login page need make reg page. 35 less

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
+import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
+import {SiteLayoutComponent} from "./shared/layouts/site-layout/site-layout.component";
 
 const routes: Routes = [
-    {path: 'login', component: LoginPageComponent}
+    {
+        path: '',
+        component: AuthLayoutComponent,
+        children:[
+            {path: 'login', component: LoginPageComponent}
+        ]
+    },
+    {
+        path: '',
+        component: SiteLayoutComponent,
+        children:[
+        ]
+    }
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginPageComponent} from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        LoginPageComponent
+        LoginPageComponent,
+        AuthLayoutComponent,
+        SiteLayoutComponent
     ],
     imports: [
         BrowserModule,
