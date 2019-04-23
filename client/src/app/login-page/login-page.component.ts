d906ef69e69eda3f9943f7b223e11b7ef61fed7c
blabuduua <31963350+blabuduua@users.noreply.github.com>
2019-04-23 12:02:45 +0300

    make auth layout

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+    selector: 'app-login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.form = new FormGroup({
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.email
+            ]),
+            password: new FormControl(null, [
+                Validators.required,
+                Validators.minLength(8)
+            ])
+        })
+    }
+
+    onSubmit
 
 }
