b53562ecb1a3c0223f51cc1e117c2fa93b439dda
blabuduua <31963350+blabuduua@users.noreply.github.com>
2019-04-26 09:35:47 +0300

    clear node

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://ora:z1f48s9f0amuqHFT@ora-zb5ug.mongodb.net/test?retryWrites=true',
-  jwt: 'B.passport'
-};
--- utils/errorsHandler.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (res, error) {
-    res.status(500).json({
-        success: false,
-        message: error.message ? error.message : error
-    });
-}
