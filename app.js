4e66532a90924bffb3e776ea1c65c405fea2b57e
blabuduua <31963350+blabuduua@users.noreply.github.com>
2019-04-26 15:11:57 +0300

    all ok

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ dotenv.config();
 
 
 // ДЛЯ ПОДКЛЮЧЕНИЯ К БАЗЕ ДАННЫХ
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
     .then(() => console.log('DB connected'));
 
 
