dc8d76b6ae1c287878542b6e96dbbb0dbadead5d
blabuduua <31963350+blabuduua@users.noreply.github.com>
2019-05-02 16:44:21 +0300

    make true status code and select method in query like _id title body

--- controllers/controllers_post.js
+++ controllers/controllers_post.js
@@ -2,12 +2,14 @@ const Post = require('../models/models_post');
 
 // ДЛЯ ЭКСПОРТА ФУНКЦИИ ВОЗВРАТА ВСЕХ ПОСТОВ
 exports.getPosts = (req, res) => {
-    res.json({
-        Posts: [
-            {FirstPost: 'FirstPost', Title: 'Title'},
-            {SecondPost: 'SecondPost', Title: 'Title'}
-        ]
-    })
+    const post = Post.find()
+        .select("_id title body")
+        .then((posts) => {
+            res.json({
+                posts
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 // ДЛЯ ЭКСПОРТА ФУНКЦИИ СОЗДАНИЯ ПОСТА
@@ -15,7 +17,7 @@ exports.createPost = (req, res) => {
     const post = new Post(req.body);
 
     post.save().then(result => {
-            res.status(200).json({
+            res.json({
                 post: result
             });
         });
