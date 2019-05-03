2e0265d42ddf6f5f139f04770bcc9a5a37ae56e0
blabuduua <31963350+blabuduua@users.noreply.github.com>
2019-05-03 09:35:49 +0300

    make hash password method

--- models/models_user.js
+++ models/models_user.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+// ДЛЯ ГЕНЕРАЦИИ СЛУЧАЙНЫХ ЧИСЕЛ
+const uuidv1 = require('uuid/v1');
+
+// ДЛЯ ШИФРОВАНИЯ
+const crypto = require('crypto');
+
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -22,5 +29,39 @@ const userSchema = new mongoose.Schema({
     },
     updated: Date
 });
+
+// virtual field
+userSchema.virtual('password')
+    .set(function (password) {
+        // create temporary variable called _password
+        this._password = password;
+
+        // generate a timestamp
+        this.salt = uuidv1();
+
+        // encryptPassword
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function () {
+        return this._password;
+    });
+
+
+// methods
+userSchema.methods = {
+    encryptPassword: function(password){
+        if(!password) return '';
+
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+
+        } catch (err) {
+            return '';
+        }
+    }
+};
+
 
 module.exports = mongoose.model('User', userSchema);
